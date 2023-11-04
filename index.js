f3df04e5659562eb0520557d1d59ace3e5d364d9
ayushchauhands <112310498+ayushchauhands@users.noreply.github.com>
2023-11-04 22:21:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// document.getElementById("count-el").innerText = 74;
+
+// camelCase
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+console.log(countEl);
+console.log(saveEl);
+
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+  console.log(count);
+}
+
+function save() {
+  let countStr = " " + count + " -";
+  saveEl.textContent += countStr;
+  console.log(count);
+  count = 0;
+  countEl.textContent = 0;
+}
